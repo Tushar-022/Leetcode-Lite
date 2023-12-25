5a86cb634a21c5cc0b8f71ed45f0543c05e1000f
Tushar-022 <khandelwaltushar2002@gmail.com>
2023-12-25 23:15:21 +0530

    thode aur  changes

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,26 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
 import AuthModal from "@/components/Modals/AuthModal";
 import Navbar from "@/components/Navbar/Navbar";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/firebase/firebase";
-import { useRecoilValue } from "recoil";
-import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
+
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = () => {
-	const authModal = useRecoilValue(authModalState);
-	const [user, loading, error] = useAuthState(auth);
-	const [pageLoading, setPageLoading] = useState(true);
-	const router = useRouter();
+  const authModal = useRecoilValue(authModalState);
+  const [user, loading, error] = useAuthState(auth);
+  const [pageLoading, setPageLoading] = useState(true);
+  const router = useRouter();
 
-	useEffect(() => {
-		if (user) router.push("/");
-		if (!loading && !user) setPageLoading(false);
-	}, [user, router, loading]);
+  useEffect(() => {
+    if (user) router.push("/");
+    if (!loading && !user) setPageLoading(false);
+  }, [user, router, loading]);
 
-	if (pageLoading) return null;
+  if (pageLoading) return null;
 
 	return (
 		<div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
